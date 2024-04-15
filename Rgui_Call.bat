9574dab6b198e64b5b4d7b08b3574b65f7beab6b
John Wallace <John.Wallace@noaa.gov>
2024-04-15 01:36:44 -0700

    Update Rgui_Call.bat

--- Rgui_Call.bat
+++ Rgui_Call.bat
@@ -6,7 +6,7 @@
        echo cat("\n\nCreating a plot from", %1, "to", %2, "with", %3, "colored points\n\n") >> .Rprofile
        echo simplePlot(%1, %2, col = %3) >> .Rprofile
        echo print(%1:%2) >> .Rprofile
-       echo Sys.sleep(12) >> .Rprofile
+       echo Sys.sleep(7) >> .Rprofile
        echo quit('no',,FALSE) >> .Rprofile
        
        echo C:\\R\\R\\bin\\x64\\Rgui.exe --no-save --no-restore --no-site-file --no-environ > run.bat
